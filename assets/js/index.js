45287eec5280bcd414829f88d104037fadbe5756
howler118 <robhowland@manx.net>
2020-11-09 13:27:29 +0000

    Added welcome text div, added css styling rules and added jquery effects

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,21 @@
-function initMap(){
-    const myHome = {lat: 54.152804, lng: -4.497640};
-    const map = new google.maps.Map(document.getElementById("map"),{
-        zoom: 10,
-        center: myHome,
-        }
-    );
+/*function initMap(){
+        const myHome = {lat: 54.152804, lng: -4.497640};
+        const map = new google.maps.Map(document.getElementById("map"),{
+            zoom: 10,
+            center: myHome,
+            }
+        );
 
 
-    }
+}*/
+
+$("#welcome-box").mouseenter(function(){
+    $("#welcome-text").slideToggle(1000);
+    $("#bradda-head").fadeTo(1000, 0.3);
+});
+
+$("#welcome-box").mouseleave(function(){
+    $("#welcome-text").slideToggle(1000);
+    $("#bradda-head").fadeTo(1000, 0.8);
+});
+
