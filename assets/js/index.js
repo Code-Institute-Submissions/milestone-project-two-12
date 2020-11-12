51d12afce9cb157c9347d2a124637bebc433529e
howler118 <robhowland@manx.net>
2020-11-12 22:30:05 +0000

    Completed 4 out of the nine flip boxes in the basic information section

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,7 +8,7 @@
 
 
 }*/
-
+/* Display the welcome text and fade the background picture */
 $("#welcome-box").mouseenter(function(){
     $("#welcome-text").slideToggle(1000);
     $("#bradda-head").fadeTo(1000, 0.3);
@@ -22,4 +22,16 @@ $("#welcome-box").mouseleave(function(){
 /* Flips the cards in the basic information section*/
 $("#capital").hover(function(){
     $("#capital-container").toggleClass("rotate");
+});
+
+$("#population").hover(function(){
+    $("#population-container").toggleClass("rotate");
+});
+
+$("#language").hover(function(){
+    $("#language-container").toggleClass("rotate");
+});
+
+$("#flag").hover(function(){
+    $("#flag-container").toggleClass("rotate");
 });
