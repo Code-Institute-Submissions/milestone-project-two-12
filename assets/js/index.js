60fe3ccc16ea9008109c41f240cda2cc26765add
howler118 <robhowland@manx.net>
2020-11-16 14:44:16 +0000

    Added restaurant and site locations to map with styling rules

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,6 +12,20 @@ function initMap(){
         ['<div class="map-info-window"><h2>The Tea Junction</h2>' + '<i class="fas fa-coffee"></i>'+ '<p><a href="https://theteajunction.im/" target="_blank">View Website</a></p>'+'<br>' +'<p><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star-half"></i></p></div>', 54.153042, -4.479047, 2],
         ['<div class="map-info-window"><h2>Silverdale Glen Cafe</h2>' + '<i class="fas fa-coffee"></i>'+ '<p><a href="https://www.facebook.com/SilverdaleGlen/" target="_blank">View on Facebook</a></p>'+'<br>' +'<p><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star-half"></i></p></div>', 54.106170, -4.638335, 1],
     ];
+
+    var restaurants = [
+        ['<div class="map-info-window"><h2>La Mona Lisa</h2>' + '<i class="fas fa-utensils"></i>'+ '<p><a href="http://www.lamonalisa.im/" target="_blank">View Website</a></p>'+'<br>' +'<p><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star-half"></i></p></div>', 54.226702, -4.392331, 4],
+        ['<div class="map-info-window"><h2>Ocean Views Bar & Grill</h2>' + '<i class="fas fa-utensils"></i>'+ '<p><a href="https://oceanviews.im/" target="_blank">View Website</a></p>'+'<br>' +'<p><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star-half"></i></p></div>', 54.169741, -4.455851, 3],
+        ['<div class="map-info-window"><h2>Portofino Restaurant</h2>' + '<i class="fas fa-utensils"></i>'+ '<p><a href="https://www.portofino.im/" target="_blank">View Website</a></p>'+'<br>' +'<p><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star-half"></i></p></div>', 54.146948, -4.484625, 2],
+        ['<div class="map-info-window"><h2>Jaks Bar and Stakehouse</h2>' + '<i class="fas fa-utensils"></i>'+ '<p><a href="https://www.jakspub.com/" target="_blank">View on Facebook</a></p>'+'<br>' +'<p><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star-half"></i></p></div>', 54.152763, -4.478363, 1],
+    ];
+
+    var sites = [
+        ['<div class="map-info-window"><h2>Castle Rushen</h2>' + '<i class="fab fa-fort-awesome"></i>'+ '<p><a href="https://manxnationalheritage.im/our-sites/castle-castletown/" target="_blank">Learn more</a></p>'+'<br>' +'<p><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star-half"></i></p></div>', 54.073717, -4.652186, 4],
+        ['<div class="map-info-window"><h2>Peel Castle</h2>' + '<i class="fab fa-fort-awesome"></i>'+ '<p><a href="https://manxnationalheritage.im/our-sites/peel-castle/" target="_blank">Learn more</a></p>'+'<br>' +'<p><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star-half"></i></p></div>', 54.226539, -4.699166, 3],
+        ['<div class="map-info-window"><h2>Laxey Wheel</h2>' + '<i class="fab fa-fort-awesome"></i>'+ '<p><a href="https://manxnationalheritage.im/our-sites/laxey-wheel/" target="_blank">Learn more</a></p>'+'<br>' +'<p><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star-half"></i></p></div>', 54.238371, -4.407430, 2],
+        ['<div class="map-info-window"><h2>Calf of Mann</h2>' + '<i class="fab fa-fort-awesome"></i>'+ '<p><a href="https://manxnationalheritage.im/visit/stay-with-us/calf-of-man-bird-observatory/" target="_blank">Learn more</a></p>'+'<br>' +'<p><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star-half"></i></p></div>', 54.060854, -4.803142, 1],
+    ];
     
     var infowindow = new google.maps.InfoWindow();
 
@@ -21,12 +35,45 @@ function initMap(){
         marker = new google.maps.Marker({
             position: new google.maps.LatLng(cafes[i][1], cafes[i][2]),
             map: map,
-            icon: "http://maps.google.com/mapfiles/kml/paddle/red-circle.png"
+            icon: "http://maps.google.com/mapfiles/kml/shapes/coffee.png"
         });
 
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
+                map.setZoom(13);
                 infowindow.setContent(cafes[i][0]);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+    }
+
+    for (i = 0; i < restaurants.length; i++) {  
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(restaurants[i][1], restaurants[i][2]),
+            map: map,
+            icon: "http://maps.google.com/mapfiles/kml/shapes/dining.png"
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                map.setZoom(13);
+                infowindow.setContent(restaurants[i][0]);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+    }
+
+    for (i = 0; i < sites.length; i++) {  
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(sites[i][1], sites[i][2]),
+            map: map,
+            icon: "http://maps.google.com/mapfiles/kml/shapes/ranger_station.png"
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                map.setZoom(13);
+                infowindow.setContent(sites[i][0]);
                 infowindow.open(map, marker);
             }
         })(marker, i));
