8ff4c41f08ab0a3920747d1e171709aca1c5897a
howler118 <robhowland@manx.net>
2020-11-30 16:28:41 +0000

    Removed test function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,211 @@
+
+/* ================================ Welcome section ================================ */
+/* Display the welcome text and fade the background picture */
+$("#welcome-box").mouseenter(function(){
+    $("#welcome-text").slideToggle(1000);
+    $("#bradda-head").fadeTo(1000, 0.3);
+});
+
+$("#welcome-box").mouseleave(function(){
+    $("#welcome-text").slideToggle(1000);
+    $("#bradda-head").fadeTo(1000, 0.8);
+});
+
+
+/* ================================ Basic info section ================================ */
+/* Flips the cards in the basic information section*/
+$("#capital").click(function(){
+    $("#capital-container").toggleClass("rotate");
+});
+
+$("#population").click(function(){
+    $("#population-container").toggleClass("rotate");
+});
+
+$("#language").click(function(){
+    $("#language-container").toggleClass("rotate");
+});
+
+$("#flag").click(function(){
+    $("#flag-container").toggleClass("rotate");
+});
+
+$("#currency").click(function(){
+    $("#currency-container").toggleClass("rotate");
+});
+
+$("#nationality").click(function(){
+    $("#nationality-container").toggleClass("rotate");
+});
+
+$("#climate").click(function(){
+    $("#climate-container").toggleClass("rotate");
+});
+
+$("#government").click(function(){
+    $("#government-container").toggleClass("rotate");
+});
+
+$("#workpermit").click(function(){
+    $("#workpermit-container").toggleClass("rotate");
+});
+
+$("#basic-info-inner").mouseleave(function(){
+    $("#capital-container").removeClass("rotate");
+    $("#population-container").removeClass("rotate");
+    $("#language-container").removeClass("rotate");
+    $("#flag-container").removeClass("rotate");
+    $("#currency-container").removeClass("rotate");
+    $("#nationality-container").removeClass("rotate");
+    $("#climate-container").removeClass("rotate");
+    $("#government-container").removeClass("rotate");
+    $("#workpermit-container").removeClass("rotate");
+});
+
+/* Fading the background on the facts section */
+$("#facts-bottom").mouseenter(function(){
+    $("#niarbyl").fadeTo(1000, 0.3);
+});
+
+$("#facts-bottom").mouseleave(function(){
+    $("#niarbyl").fadeTo(1000, 0.8);
+});
+
+/* ================================ Did you know section, guess the year IoM gave women the right to vote ================================ */
+guess = function(year) {
+    $("#guess").click(function() {
+        $("#instructions").css("display","none");
+        $("#answer-text").css("display","none");
+        $("#giveUp").css("display","none");
+        $("#form-1-year").css("display","block");
+        
+        var value = document.getElementById("form-1");
+        var year = "";
+
+        var i; /* Used code from W3Schools to get this function working (https://www.w3schools.com/js/tryit.asp?filename=tryjs_form_elements) */
+        for (i = 0; i < value.length ;i++) {
+            year += value.elements[i].value;
+            
+            if(year == 1881) {
+                document.getElementById("guess-output").innerHTML = "That's correct."
+                guess("That's correct.");
+            }
+
+            else if(year < 1 && year > -1) {
+                document.getElementById("guess-output").innerHTML = "Come on, have a guess!";
+                return console.log("Come on, have a guess!");
+            }
+
+            else if(year > 2020) {
+                document.getElementById("guess-output").innerHTML = "Hint: It's already happened!";
+                return console.log("Hint: It's already happened!");
+            }
+
+            else if(year > 2100) {
+                document.getElementById("guess-output").innerHTML = "Now you are being silly";
+            }
+
+            else if(year < 0 ) {
+                document.getElementById("guess-output").innerHTML = "Can't have a negative year!";
+            }
+
+            else if(year - 1881 < 2 && year - 1881 > -2 ) {
+                document.getElementById("guess-output").innerHTML = "So close! You're within one year";
+            }
+
+            else if(year - 1881 < 3 && year - 1881 > -3 ) {
+                document.getElementById("guess-output").innerHTML = "Almost! You're within two years";
+            }
+
+            else if(year - 1881 < 4 && year - 1881 > -4 ) {
+                document.getElementById("guess-output").innerHTML = "Good guess! You're within three years";
+            }
+
+            else if(year - 1881 < 5 && year - 1881 > -5 ) {
+                document.getElementById("guess-output").innerHTML = "Good guess! You're within four years";
+            }
+
+            else if(year - 1881 < 6 && year - 1881 > -6 ) {
+                document.getElementById("guess-output").innerHTML = "Almost! You're within five years";
+            }
+
+            else if(year - 1881 < 11 && year - 1881 > -11 ) {
+                document.getElementById("guess-output").innerHTML = "You're within ten years";
+            }
+
+            else if(year - 1881 < 21 && year - 1881 > -21 ) {
+                document.getElementById("guess-output").innerHTML = "You're within twenty years";
+            }
+
+            else if(year - 1881 < 31 && year - 1881 > -31 ) {
+                document.getElementById("guess-output").innerHTML = "You're within thirty years";
+            }
+
+            else if(year - 1881 < 41 && year - 1881 > -41 ) {
+                document.getElementById("guess-output").innerHTML = "You're within fourty years";
+            }
+
+            else if(year - 1881 < 51 && year - 1881 > -51 ) {
+                document.getElementById("guess-output").innerHTML = "You're within fifty years";
+            }
+
+            else if(year < 1832 ) {
+                document.getElementById("guess-output").innerHTML = "Much later. Try again";
+            }
+
+            else if(year > 1930) {
+                document.getElementById("guess-output").innerHTML = "Much earlier. Try again";
+            }
+
+            else {
+                document.getElementById("guess-output").innerHTML = "Please enter a valid year";
+            }
+        }
+    })
+}
+
+/* Reveal Answer */
+$("#answer").click(function(){
+    $("#form-1-year").css("display","none");
+    $("#instructions").css("display","none");
+    $("#giveUp").css("display","block");
+    $("#answer-text").css("display","block");
+    document.getElementById("guess-output").innerHTML = "";  
+});
+
+/* Reset the question so they can guess again */
+$("#reset").click(function(){
+    $("#giveUp").css("display","none");
+    $("#instructions").css("display","block");
+    $("#form-1-year").css("display","block");
+    $("#answer-text").css("display","none");
+    document.getElementById("guess-output").innerHTML = "";
+    document.getElementById("form-1").reset(); 
+});
+
+
+/* ================================ Things to do section ================================ */
+ /* Map */
 function initMap(){
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
     var mapCenter = {lat: 54.236107, lng: -4.548056};
     var map = new google.maps.Map(document.getElementById("map"),{
         zoom: 10,
         center: mapCenter,
         }
     );
-    
+    directionsRenderer.setMap(map);
+
+    const onChangeHandler = function () {
+        calculateAndDisplayRoute(directionsService, directionsRenderer);
+    };
+    document.getElementById("from").addEventListener("change", onChangeHandler);
+    document.getElementById("to").addEventListener("change", onChangeHandler);
+
+    var Douglas = new google.maps.LatLng(54.153042, -4.479047);
+    var Onchan = new google.maps.LatLng(54.169741, -4.455851);
+
     const locations = [
         { lat: 54.063889, lng: -4.792477 }, /* (A) The Cafe At The Sound */
         { lat: 54.223635, lng: -4.393898 }, /* (B) The Shed */
@@ -46,20 +246,10 @@ function initMap(){
         label: labels[i % labels.length],
         content: locationInfo[i]
         });
-    }); 
-
-     
+    });  
 
 
 /*
-    
-
-
-
-
-
-
-
     var cafes = [
         ['<div class="map-info-window"><h2>The Cafe At The Sound</h2>' + '<i class="fas fa-coffee"></i>'+ '<p><a href="https://www.thesound.im/" target="_blank">View Website</a></p>'+'<br>' +'<p><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star-half"></i></p></div>', 54.063889, -4.792477, 4],
         ['<div class="map-info-window"><h2>The Shed</h2>' + '<i class="fas fa-coffee"></i>'+ '<p><a href="https://www.facebook.com/theshedlaxey/" target="_blank">View on Facebook</a></p>'+'<br>' +'<p><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star-half"></i></p></div>', 54.223635, -4.393898, 3],
@@ -136,190 +326,29 @@ function initMap(){
     });
 }
 
-
-
-/* Display the welcome text and fade the background picture */
-$("#welcome-box").mouseenter(function(){
-    $("#welcome-text").slideToggle(1000);
-    $("#bradda-head").fadeTo(1000, 0.3);
-});
-
-$("#welcome-box").mouseleave(function(){
-    $("#welcome-text").slideToggle(1000);
-    $("#bradda-head").fadeTo(1000, 0.8);
-});
-
-
-
-/* Flips the cards in the basic information section*/
-$("#capital").click(function(){
-    $("#capital-container").toggleClass("rotate");
-});
-
-$("#population").click(function(){
-    $("#population-container").toggleClass("rotate");
-});
-
-$("#language").click(function(){
-    $("#language-container").toggleClass("rotate");
-});
-
-$("#flag").click(function(){
-    $("#flag-container").toggleClass("rotate");
-});
-
-$("#currency").click(function(){
-    $("#currency-container").toggleClass("rotate");
-});
-
-$("#nationality").click(function(){
-    $("#nationality-container").toggleClass("rotate");
-});
-
-$("#climate").click(function(){
-    $("#climate-container").toggleClass("rotate");
-});
-
-$("#government").click(function(){
-    $("#government-container").toggleClass("rotate");
-});
-
-$("#workpermit").click(function(){
-    $("#workpermit-container").toggleClass("rotate");
-});
-
-$("#basic-info-inner").mouseleave(function(){
-    $("#capital-container").removeClass("rotate");
-    $("#population-container").removeClass("rotate");
-    $("#language-container").removeClass("rotate");
-    $("#flag-container").removeClass("rotate");
-    $("#currency-container").removeClass("rotate");
-    $("#nationality-container").removeClass("rotate");
-    $("#climate-container").removeClass("rotate");
-    $("#government-container").removeClass("rotate");
-    $("#workpermit-container").removeClass("rotate");
-});
-
-/* Fading the background on the facts section */
-$("#facts-bottom").mouseenter(function(){
-    $("#niarbyl").fadeTo(1000, 0.3);
-});
-
-$("#facts-bottom").mouseleave(function(){
-    $("#niarbyl").fadeTo(1000, 0.8);
-});
-
-/* Did you know section, guess the year IoM gave women the right to vote */
-
-$("#guess").click(function() {
-    $("#instructions").css("display","none");
-    $("#answer-text").css("display","none");
-    $("#giveUp").css("display","none");
-    $("#form-1-year").css("display","block");
-
-    var value = document.getElementById("form-1");
-    var year = "";
-
-    var i; /* Used code from W3Schools to get this function working (https://www.w3schools.com/js/tryit.asp?filename=tryjs_form_elements) */
-    for (i = 0; i < value.length ;i++) {
-        year += value.elements[i].value;
-        
-        if(year == 1881) {
-            document.getElementById("guess-output").innerHTML = "That's correct.";
-        }
-
-        else if(year < 1 && year > -1) {
-            document.getElementById("guess-output").innerHTML = "Come on, have a guess!";
-        }
-
-        else if(year > 2020) {
-            document.getElementById("guess-output").innerHTML = "Hint: It's already happened!";
-        }
-
-        else if(year > 2100) {
-            document.getElementById("guess-output").innerHTML = "Now you are being silly";
-        }
-
-        else if(year < 0 ) {
-            document.getElementById("guess-output").innerHTML = "Can't have a negative year!";
-        }
-
-        else if(year - 1881 < 2 && year - 1881 > -2 ) {
-            document.getElementById("guess-output").innerHTML = "So close! You're within one year";
-        }
-
-        else if(year - 1881 < 3 && year - 1881 > -3 ) {
-            document.getElementById("guess-output").innerHTML = "Almost! You're within two years";
-        }
-
-        else if(year - 1881 < 4 && year - 1881 > -4 ) {
-            document.getElementById("guess-output").innerHTML = "Good guess! You're within three years";
-        }
-
-        else if(year - 1881 < 5 && year - 1881 > -5 ) {
-            document.getElementById("guess-output").innerHTML = "Good guess! You're within four years";
-        }
-
-        else if(year - 1881 < 6 && year - 1881 > -6 ) {
-            document.getElementById("guess-output").innerHTML = "Almost! You're within five years";
-        }
-
-        else if(year - 1881 < 11 && year - 1881 > -11 ) {
-            document.getElementById("guess-output").innerHTML = "You're within ten years";
-        }
-
-        else if(year - 1881 < 21 && year - 1881 > -21 ) {
-            document.getElementById("guess-output").innerHTML = "You're within twenty years";
-        }
-
-        else if(year - 1881 < 31 && year - 1881 > -31 ) {
-            document.getElementById("guess-output").innerHTML = "You're within thirty years";
-        }
-
-        else if(year - 1881 < 41 && year - 1881 > -41 ) {
-            document.getElementById("guess-output").innerHTML = "You're within fourty years";
-        }
-
-        else if(year - 1881 < 51 && year - 1881 > -51 ) {
-            document.getElementById("guess-output").innerHTML = "You're within fifty years";
-        }
-
-        else if(year < 1832 ) {
-            document.getElementById("guess-output").innerHTML = "Much later. Try again";
-        }
-
-        else if(year > 1930) {
-            document.getElementById("guess-output").innerHTML = "Much earlier. Try again";
-        }
-
-        else {
-            document.getElementById("guess-output").innerHTML = "Please enter a valid year";
-        }
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  directionsService.route(
+    {
+      origin: {
+        query: document.getElementById("from").value,
+      },
+      destination: {
+        query: document.getElementById("to").value,
+      },
+      travelMode: google.maps.TravelMode.DRIVING,
+    },
+    (response, status) => {
+      if (status === "OK") {
+        directionsRenderer.setDirections(response);
+      } else {
+        window.alert("Directions request failed due to " + status);
+      }
     }
-})
-
-/* Reveal Answer */
-$("#answer").click(function(){
-    $("#form-1-year").css("display","none");
-    $("#instructions").css("display","none");
-    $("#giveUp").css("display","block");
-    $("#answer-text").css("display","block");
-    document.getElementById("guess-output").innerHTML = "";  
-});
-
-/* Reset the question so they can guess again */
-$("#reset").click(function(){
-    $("#giveUp").css("display","none");
-    $("#instructions").css("display","block");
-    $("#form-1-year").css("display","block");
-    $("#answer-text").css("display","none");
-    document.getElementById("guess-output").innerHTML = "";
-    document.getElementById("form-1").reset(); 
-});
+  );
+}
 
 
-
-/* Location info */
+/* Map buttons */
 $("#button-a").click(function(){
     $(".location").css("display","none");
     $("#sound-cafe").css("display","inline-block");
@@ -381,7 +410,7 @@ $("#button-l").click(function(){
     $("#calf-of-man").css("display","inline-block");
 });
 
-/* Fading the background on the map section */
+/*  Fading the background on the map section  */
 $("#map-container").mouseenter(function(){
     $("#calf-of-man-2").fadeTo(1000, 0.3);
 });
@@ -390,12 +419,8 @@ $("#map-container").mouseleave(function(){
     $("#calf-of-man-2").fadeTo(1000, 0.8);
 });
 
-
-function guessYear(){
-    return 10;
-}
-
-/* Fading the background on the contact section */
+/* ================================ Contact section ================================ */
+/* Fading the background on the contact section  */
 $("#contact-form-container").mouseenter(function(){
     $("#dragon-rock").fadeTo(1000, 0.3);
 });
